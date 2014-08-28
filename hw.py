e7de7d84a7aaac61ae8c58f56eec36d300c0f2fb
kogos <stephen@aims.edu.gh>
2014-08-28 16:39:54 +0000

    example

--- hw.py
+++ hw.py
@@ -1,5 +1,13 @@
 __author__ = 'stephen'
 print("Hello world")
 #print("This is not what i needed")
-print ("Hello Ghana")
-print ("This is not easy")
+print("Hello Ghana")
+print("This is not easy")
+#---------Declaring Variables-------
+text=("name1")
+
+#---------DeclaringFunctions--------
+
+#---------Printing variables--------
+print(text)
+#---------Printing functions--------
