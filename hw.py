b53448ffe6c4cc36073f5660842da201a4dc9f54
kogos <stephen@aims.edu.gh>
2014-08-28 20:54:16 +0000

    Functions and variables

--- hw.py
+++ hw.py
@@ -1,18 +1,35 @@
 __author__ = 'stephen'
-print("Hello world")
-#print("This is not what i needed")
-print("Hello Ghana")
-print("This is not easy")
-#---------Declaring Variables-------
-text=("Student name")
-#---------List--------
-
-#---------DeclaringFunctions--------
+from numpy import *
+#---------DECLARING VARIABLES-------
+text=("Student name")#string variable
+star=("Hello Ghana")
+y=50#Numeric variable
+x=20
+print(text)#printing contents of variables(text)
+print(star)
+print(x+y)
+#---------DECLARING FUNCTIONS--------
+#Prime numbers
 def prime(a,b):
+    print("The prime numbers between ",a," and ",b," are:")
     for x in range(a,b):
         if all(x%i!=0 for i in range(2,x)):
             print(x)
-#---------Printing variables--------
-print(text)
-#---------Calling functions--------
-prime(2,100)
+prime(2,50)#Calling the prime function
+#Equation of a line
+def straightline(x1,x2,y1,y2,c):
+    m=((y2-y1)/(x2-x1))
+    print("Equation: y=",m,"x +",c)
+straightline(2.4,1,8,5,2.8)#Calling the straightline function
+#Area of a triangle
+def triangle(a,b,c):
+    s=(a+b+c)/2
+    area=sqrt(s*(s-a)*(s-b)*(s-c))
+    print("The area of the triangle is: ",area,"sq. units")
+triangle(5,12,13)
+#---------LISTS---------------------===
+listdata=["numbers","functions","variables"]
+print(listdata[2]) #Indexing starts at zero, element[1] is in position 2.
+
+
+
