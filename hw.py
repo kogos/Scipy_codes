c2a83d0d453d1769667e38707603c54e0199f9a5
kogos <stephen@aims.edu.gh>
2014-08-28 15:10:40 +0000

    trial one

--- hw.py
+++ hw.py
@@ -2,4 +2,4 @@ __author__ = 'stephen'
 print("Hello world")
 #print("This is not what i needed")
 print ("Hello Ghana")
-print ("This is not an easy task")
+print ("This is not an easy task my friends")
