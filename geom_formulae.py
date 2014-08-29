68b0187e4fc61560c87ff8b9897483840b8b2609
kogos <stephen@aims.edu.gh>
2014-08-29 15:10:58 +0000

    Trial2

--- geom_formulae.py
+++ geom_formulae.py
@@ -1,6 +1,6 @@
 __author__ = 'stephen'
+from numpy import *
 from numbers import Number
-steve = "sst"
 
 
 def square_perimeter(side: Number) ->Number:
@@ -12,7 +12,7 @@ def square_perimeter(side: Number) ->Number:
     16
     """
     return 4*side
-print(square_perimeter(steve))
+print(square_perimeter(4))
 
 
 def rectangle_area(length: Number, width: Number) ->Number:
@@ -25,5 +25,30 @@ def rectangle_area(length: Number, width: Number) ->Number:
     50
     """
     return length * width
+print(rectangle_area(10.5, 5.2))
 
-print(rectangle_area(10,5))
+
+def solid_cone_surface_area(radius: Number, slant_height: Number) ->Number:
+    """
+    It calculates the area of a rectangle given length and width
+    :param radius: base radius of the cone
+    :param slant_height: slant height of the cone
+    :return: the surface area of the solid cone (in square units of input variables)
+    >>> solid_cone_surface_area(4,9)
+    163.36281798666926
+    """
+    return pi*radius*radius+pi*radius*slant_height
+print(solid_cone_surface_area(4, 9))
+
+
+def sphere_volume(radius: Number) ->Number:
+    """
+    Calculates the volume of a sphere.
+    :param radius: radius of the sphere
+    :return: the volume of the sphere (in cubic units of the radius)
+    >>> sphere_volume(7)
+    1436.7550402417319
+    """
+    return 4*pi*radius*radius*radius/3
+print(sphere_volume(7))
+
