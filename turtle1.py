e7bb7aa8b1c25b6839e1c9d81baac7d2f1564ad8
kogos <stephen@aims.edu.gh>
2014-09-01 21:21:30 +0000

    Final

--- turtle1.py
+++ turtle1.py
@@ -3,11 +3,13 @@ import turtle
 from matplotlib.patches import Wedge
 import matplotlib.pyplot as plt
 from numpy import *
+#------------------------------CIRCLE------------------------
 
 
 def draw_circle(radius):
     turtle.circle(radius)
     turtle.done()
+#------------------------------RECTANGLE----------------------
 
 
 def draw_rectangle(width, length):
@@ -17,8 +19,9 @@ def draw_rectangle(width, length):
         turtle.left(90)
         turtle.forward(width)
         turtle.left(90)
-        i = i + 1
+        i = i+1
     turtle.done()
+#------------------------------TRAPEZIUM----------------------
 
 
 def draw_trapezium(parallel_side_1, parallel_side_2, slant_height_1, slant_height_2):
@@ -31,6 +34,7 @@ def draw_trapezium(parallel_side_1, parallel_side_2, slant_height_1, slant_heigh
     turtle.forward(slant_height_2)
     turtle.left(135)
     turtle.done()
+#------------------------------TRIANGLE------------------------
 
 
 def draw_triangle(side):
@@ -40,6 +44,7 @@ def draw_triangle(side):
         turtle.left(120)
         i = i+1
     turtle.done()
+#------------------------------SEMI CIRCLE----------------------
 
 
 def draw_semi_circle(radius):
@@ -48,11 +53,13 @@ def draw_semi_circle(radius):
     paths = Wedge((0.5, 0), radius, 0, 180)
     ax.add_artist(paths)
     plt.show()
+#===============================================================
 
 
 if __name__ == "__main__":
     draw_circle(50)
     draw_rectangle(50, 100)
+    draw_trapezium(200, 100, sqrt(5000), sqrt(5000))
     draw_triangle(70)
     draw_semi_circle(0.5)
-    draw_trapezium(200, 100, sqrt(5000), sqrt(5000))
+#============================END================================
