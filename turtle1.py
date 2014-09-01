7d91962fd3016d12ff91a9c9934cac894d1bdbdf
kogos <stephen@aims.edu.gh>
2014-09-01 16:43:15 +0000

    trial2

--- turtle1.py
+++ turtle1.py
@@ -1,38 +1,58 @@
 __author__ = 'stephen'
 import turtle
+from matplotlib.patches import Wedge
+import matplotlib.pyplot as plt
 from numpy import *
 
 
-def draw_square(side):
+def draw_circle(radius):
+    turtle.circle(radius)
+    turtle.done()
+
+
+def draw_rectangle(width, length):
     i = 0
-    while (i < 4):
+    while i < 2:
+        turtle.forward(length)
+        turtle.left(90)
+        turtle.forward(width)
+        turtle.left(90)
+        i = i + 1
+    turtle.done()
+
+
+def draw_trapezium(parallel_side_1, parallel_side_2, slant_height_1, slant_height_2):
+    turtle.forward(parallel_side_1)
+    turtle.left(135)
+    turtle.forward(slant_height_1)
+    turtle.left(45)
+    turtle.forward(parallel_side_2)
+    turtle.left(45)
+    turtle.forward(slant_height_2)
+    turtle.left(135)
+    turtle.done()
+
+
+def draw_triangle(side):
+    i = 0
+    while i < 3:
         turtle.forward(side)
-        turtle.left(90)
+        turtle.left(120)
         i = i+1
-
-side = 50
-while side in range(50,101):
-    draw_square(side)
-    side = side + 10
-turtle.done()
+    turtle.done()
 
 
-def draw_rectangle(length, width):
-    i = 0
-    while i < 5:
-        if i == 0 | i == 2:
-            turtle.forward(length)
-        else:
-            turtle.forward(width)
-        turtle.left(90)
-        i = i+1
-length = 100
-width = 40
-draw_rectangle(length, width)
-turtle.done()
-"""
-while length in range(50,101):
-    draw_rectangle(length, width)
-    length = length + 10
-turtle.done()
-"""
+def draw_semi_circle(radius):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    paths = Wedge((0.5, 0), radius, 0, 180)
+    ax.add_artist(paths)
+    plt.show()
+
+
+if __name__ == "__main__":
+    draw_circle(50)
+    draw_rectangle(50, 100)
+    draw_triangle(70)
+    draw_semi_circle(0.5)
+    draw_trapezium(200,100, sqrt(5000), sqrt (5000))
